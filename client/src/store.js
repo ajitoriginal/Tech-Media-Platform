ba1f131218955ac2caa12536fa1a7fe80625f00c
Ajit Singh <86841688+ajitoriginal@users.noreply.github.com>
2025-01-12 01:56:41 +0530

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware } from '@reduxjs/toolkit';
 import { configureStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import {thunk} from 'redux-thunk';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import setAuthToken from './utils/setAuthToken';
 
