2a99494db2c26a0e7800bd11022fb675276909cb
Ajit Singh <86841688+ajitoriginal@users.noreply.github.com>
2025-01-12 04:00:49 +0530

    Update Landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -13,9 +13,9 @@ const Landing = ({ isAuthenticated }) => {
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
-          <h1 className='x-large'>Developer Media</h1>
+          <h1 className='x-large'>Welcome to Tech Media</h1>
           <p className='lead'>
-            Create a developer profile/portfolio, share posts and get help from
+            Create your developer profile/portfolio, share posts and get help from
             other developers
           </p>
           <div className='buttons'>
